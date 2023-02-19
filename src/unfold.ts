b5d199e912aac3c24d8ca341cbee34907a48763c
free-bots <free-bots.to@protonmail.com>
2023-02-19 16:44:37 +0100

    added unfolding functionality

---
+++ src/unfold.ts
@@ -0,0 +1,52 @@
+import { commands, Range, TextDocument, TextEdit, window, workspace, WorkspaceEdit } from "vscode";
+
+export const unfold = async () => {
+    const { activeTextEditor } = window;
+    const document = activeTextEditor?.document;
+    if (!document) {
+        return;
+    }
+
+    const transformedLines = transformLines(document);
+    let newText;
+    try {
+        newText = JSON.stringify(transformedLines);
+    } catch (error) {
+        console.error('Error while stringify transformedLines', error);
+        return;
+    }
+
+    const edit = new WorkspaceEdit();
+    edit.set(document.uri, [
+        TextEdit.replace(
+            new Range(
+                0,
+                0,
+                document.lineCount - 1,
+                document.lineAt(document.lineCount - 1).text.length
+            ),
+            newText
+        )
+    ]);
+
+    await workspace.applyEdit(edit);
+    await commands.executeCommand('editor.action.formatDocument.multiple'); // ask user for preferred formatter
+};
+
+const transformLines = (document: TextDocument): string[] => {
+    const lines = [];
+    for (let i = 0; i < document.lineCount; i++) {
+        const line = document.lineAt(i);
+        if (line.isEmptyOrWhitespace) {
+            continue;
+        }
+
+        try {
+            lines.push(JSON.parse(line.text));
+        } catch (error) {
+            console.error(`Error while parsing line: ${line.text}`, error);
+        }
+    }
+
+    return lines;
+};
